f4982ea4f271db657ce18063e621730a66a14beb
charlie <charlie@hotmail.com>
2026-04-19 11:28:46 +0100

    update

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
 provider "aws" {
   region = "eu-west-2" # London region
 }
+resource "aws_s3_bucket" "terraformjanbucketsun" {
+  bucket = "terraformjanbucketsun"
+}
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "terraformjanbucketsun"
+    key            = "global/s3/terraform.tfstate"
+    region         = "eu-west-2"
+  }
+}
